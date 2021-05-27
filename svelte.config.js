c5b5597a9e8b563806209571cb171019521005b9
bellice <bellice@tuta.io>
2021-05-27 11:14:59 +0200

    update package.json

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,19 @@
 import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-static';
+
+export default {
+	kit: {
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
+	}
+};
+
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export const config = {
 	preprocess: [
 		preprocess({
 			postcss: true
@@ -12,4 +25,3 @@ const config = {
 	}
 };
 
-export default config;
