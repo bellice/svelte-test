24f2d100458beeb2188e31861625e1a47a6de847
bellice <bellice@tuta.io>
2021-06-30 00:32:38 +0200

    config adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,6 @@
 import preprocess from "svelte-preprocess";
 import adapter from '@sveltejs/adapter-netlify';
 
-
-import { join } from 'path';
-import { readFileSync } from 'fs';
-import { cwd } from 'process';
-
-const pkg = JSON.parse(readFileSync(join(cwd(), 'package.json')));
-
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [
@@ -21,16 +13,8 @@ const config = {
 	],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
 		adapter: adapter(),
-		vite: {
-			ssr: {
-			noExternal: Object.keys(pkg.dependencies || {})
-			},
-			optimizeDeps: {
-				include: ["d3"]
-			}
-		}
+		target: '#svelte',
 	}
 };
 
